4403d4c0480e08872d4615443de6af3cedad6af5
Roy Ivy III <rivy.dev@gmail.com>
2025-03-13 13:33:37 -0500

    change ~ add CDN vendor expanded deps

---
+++ tests/$deps.(vendor-cdn).ts
@@ -0,0 +1,2 @@
+// surrealDB-bins
+import _7zip from 'https://cdn.jsdelivr.net/gh/rivy-t/deno.vendor-storage@2de719db3a/vendor/deno@1.44.4-vendor/esm.sh/7zip-min@2.0.0.js'; // from <'https://esm.sh/7zip-min@2.0.0'>
