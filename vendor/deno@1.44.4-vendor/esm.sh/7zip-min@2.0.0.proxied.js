b4f11edf4c136579108610438f8bf560f48675be
Roy Ivy III <rivy.dev@gmail.com>
2025-03-13 12:24:10 -0500

    change ~ add/rewrite vendor dependencies
    ```shell
    rm -r vendor/deno@1.44.4-vendor
    deno-v1.44.4 vendor --output vendor/deno@1.44.4-vendor --no-config ./$deps.ts
    cp -r vendor/.patch/* vendor/deno@1.44.4-vendor
    deno run -A tools/rewrite-vendor.deno.ts
    deno fmt --no-config vendor/deno@1.44.4-vendor
    ```

---
+++ vendor/deno@1.44.4-vendor/esm.sh/7zip-min@2.0.0.proxied.js
@@ -0,0 +1,4 @@
+/* esm.sh - 7zip-min@2.0.0 */
+import "../esm.sh/7zip-bin@5.1.1/denonext/7zip-bin.mjs";
+export * from "../esm.sh/7zip-min@2.0.0/denonext/7zip-min.mjs";
+export { default } from "../esm.sh/7zip-min@2.0.0/denonext/7zip-min.mjs";
