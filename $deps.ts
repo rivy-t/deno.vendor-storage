520c3b1c6e38b4666c19825e9c68d72ebca19f98
Roy Ivy III <rivy.dev@gmail.com>
2025-03-30 17:02:20 -0500

    maint/deps ~ add 'chalk'-related deps

--- $deps.ts
+++ $deps.ts
@@ -1,2 +1,18 @@
 // surrealDB-bins
 import _7zip from 'https://esm.sh/7zip-min@2.0.0';
+
+// chalk
+import _chalk from 'https://esm.sh/chalk@5.4.1';
+import _chalk_cli from 'https://esm.sh/chalk@5.0.1';
+
+import _ansi_regex from 'https://esm.sh/ansi-regex@6.1.0';
+import _ansi_styles from 'https://esm.sh/ansi-styles@6.2.1';
+import _cli_truncate from 'https://esm.sh/cli-truncate@4.0.0';
+import _has_ansi from 'https://esm.sh/has-ansi@6.0.0';
+import _slice_ansi from 'https://esm.sh/slice-ansi@7.1.0';
+import _string_width from 'https://esm.sh/string-width@7.2.0';
+import _strip_ansi from 'https://esm.sh/strip-ansi@7.1.0';
+import _wrap_ansi from 'https://esm.sh/wrap-ansi@9.0.0';
+
+import _chalk_table from 'https://esm.sh/chalk-table@1.0.2';
+import _chalk_template from 'https://esm.sh/chalk-template@1.1.0';
